d0c749e17ca0db1eb37c8676a60f8c09eb2ad05b
Danilo Mendonça <danilomendonca08@gmail.com>
2023-08-30 16:02:45 -0300

    Criação da Div e Input estilizados com styled components

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,8 +1,27 @@
 import styled from "styled-components"
+import Lupa from "../../assets/LupaDePesquisa.svg"
 
 export const StyledHeader = styled.header`
     background-color: #E2E2FF;
     width: 100vw;
     padding: 30px 0;
 
+`
+
+export const StyledDivHeader = styled.div`
+    display: flex; 
+    justify-content: space-between;
+    align-items: center;
+` 
+
+export const StyledInputHeader = styled.input`
+    width: 400px;
+    height: 35px;
+    border-radius: 150px;
+    background-image: url(${Lupa});
+    background-position: 3%;
+    background-repeat: no-repeat;
+    border: 1px solid white;
+    text-align: center;
+    
 `
