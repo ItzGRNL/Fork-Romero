f0d33ca2ce184341b557b55e507a18c50c9b9c01
Danilo Mendonça <danilomendonca08@gmail.com>
2023-08-30 15:59:55 -0300

    Criação do parágrafo estilizado com styled components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,17 @@
 import { StyledFooter } from "./Footer.style"
+import { StyledParagraphFooter } from "./Footer.style"
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <p className="texto-footer">
+            <StyledParagraphFooter>
             FIAP - 1TDSPX
             Nome: Danilo Araujo Mendonça / RM: 99752
             Nome: Guilherme Romero Nogueira / RM: 97696
             Nome: Matheus Andrade da Silva / RM: 98398
             Nome: Rodrigo Gonçalves Teixeira Filho / RM: 99838
             Nome: Arthur Lima Nascimento / RM: 551054
-            </p>
+            </StyledParagraphFooter>
         </StyledFooter>
     )
 }
