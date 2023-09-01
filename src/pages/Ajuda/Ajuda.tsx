f8847544bb49d80c06719f7bb6c6b58d8c1a34ce
Danilo Mendonça <danilomendonca08@gmail.com>
2023-08-31 23:36:33 -0300

    Criado a pasta da pagina 'Ajuda'

---
+++ src/pages/Ajuda/Ajuda.tsx
@@ -0,0 +1,15 @@
+import { BodySolucoes } from "../../components/BodySolucoes/BodySolucoes";
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+import imgAjuda from "../../assets/ImagemAjuda.svg"
+
+export const paragrafo = "Nesta área, pensamos em ser uma parte mais abrangente para qualquer problema que o assegurado queira reportar, gerenciado por uma inteligência, ela aconselhará o assegurado e o guiará para fazer com que ele faça a melhor escolha. Por esta área ser mais abrangente, ela estará preparada para muitas possibilidades que o assegurado traga para resolver, fazendo com que seja aconselhado a melhor escolha entre todas as possiveis."
+
+export const Ajuda = () => {
+    return ( <>
+        <Header lupa/>
+        <BodySolucoes titulo = {"Ajuda"} img = {imgAjuda} texto ={paragrafo} descricao="Imagem ilustrando uma ajuda"/>
+        <Footer />
+        </>)
+   
+}
