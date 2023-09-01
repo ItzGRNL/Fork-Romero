882d08fb4fb85947437f742334e2aa90a7a9df55
Danilo Mendonça <danilomendonca08@gmail.com>
2023-08-31 23:37:47 -0300

    Criado o componente de BodySolucoes

---
+++ src/components/BodySolucoes/BodySolucoes.style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const StyledBodySolucoes = styled.body`
+    width: 100vw;
+`
+
+export const StyledH1BodySolucoes = styled.h1`
+    font-family: 'Open Sans';
+    font-size: 32px;
+    font-weight: 300;
+    padding: 25px 0;
+`
+
+export const StyledImgBodySolucoes = styled.img`
+    max-width: 100%
+`
+export const StyledDivBodySolucoes = styled.div`
+    display: grid;
+    justify-items: center;
+    padding: 0 80px;
+`
+
+export const StyledPBodySolucoes = styled.p`
+    font-family: 'Open Sans';
+    font-size: 20px;
+    font-weight: 300;
+    padding-top: 25px;
+`
