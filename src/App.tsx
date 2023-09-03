8eb6a351e0083c6e2c93cc59ed27d32a58d43b65
Danilo Mendonça <danilomendonca08@gmail.com>
2023-09-02 21:23:25 -0300

    Alterações a fim de testes e melhorias na responsividade do site

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 import { BodySolucoes } from './components/BodySolucoes/BodySolucoes'
-import './App.css'
 import {Ajuda} from './pages/Ajuda/Ajuda'
+import { SolucaoPorto } from './pages/SolucaoPorto/SolucaoPorto'
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Ajuda />
+    <SolucaoPorto />
     {/* <Header />
     <BodySolucoes />
     <Footer /> */}
