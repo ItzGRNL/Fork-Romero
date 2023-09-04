c99379ab5fa62607b35844a7354bd8a3f37472cd
Danilo Mendonça <danilomendonca08@gmail.com>
2023-09-03 22:27:27 -0300

    Mudanças para testes e ajuste de reponsividade

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 import { BodySolucoes } from './components/BodySolucoes/BodySolucoes'
 import {Ajuda} from './pages/Ajuda/Ajuda'
+import { Organograma } from './pages/Organograma/Organograma'
 import { SolucaoPorto } from './pages/SolucaoPorto/SolucaoPorto'
 import './App.css'
 
@@ -12,7 +13,7 @@ function App() {
 
   return (
     <>
-    <SolucaoPorto />
+    <Organograma />
     {/* <Header />
     <BodySolucoes />
     <Footer /> */}
