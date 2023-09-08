da8bfef25f7778e8926abd0fb7975d926ce4bbae
Danilo Mendonça <danilomendonca08@gmail.com>
2023-09-08 19:06:42 -0300

    Media querie feito para a pagina do Organograma

--- src/components/BodyOrganograma/BodyOrganograma.tsx
+++ src/components/BodyOrganograma/BodyOrganograma.tsx
@@ -1,17 +1,18 @@
 import imgOrganograma from "../../assets/Organograma.svg";
 import { StyledDivOrganograma } from "./BodyOrganograma.style";
 import { StyledH1Organograma } from "./BodyOrganograma.style";
-import { StyledBodyOrganograma } from "./BodyOrganograma.style";
+import { StyledImgOrganograma } from "./BodyOrganograma.style";
+
 
 export const BodyOrganograma = () => {
     return(
         <>
-        <StyledBodyOrganograma>
+        <body>
             <StyledDivOrganograma>
                 <StyledH1Organograma>Organograma</StyledH1Organograma>
-                <img src={imgOrganograma} alt="Imagem de um organograma feito para o projeto" />
+                <StyledImgOrganograma src={imgOrganograma} alt="Imagem de um organograma feito para o projeto" />
             </StyledDivOrganograma>
-        </StyledBodyOrganograma>
+        </body>
         </>
     )
 }
